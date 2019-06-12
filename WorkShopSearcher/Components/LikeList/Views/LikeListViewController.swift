3da0eb8fec921ee8712cf94a4abf4f1c99241943
ryoya_ito <horusu.dqx@gmail.com>
2019-06-12 14:27:08 +0900

    いいねしたイベント一覧画面初期化

---
+++ WorkShopSearcher/Components/LikeList/Views/LikeListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LikeListViewController.swift
+//  WorkShopSearcher
+//
+//  Created by 伊藤凌也 on 2019/06/12.
+//  Copyright © 2019 ry-itto. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+final class LikeListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
