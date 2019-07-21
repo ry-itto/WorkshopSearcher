65a0f4f600bdf41d32003079e83ab91a4d5051c9
ryoya_ito <horusu.dqx@gmail.com>
2019-07-21 20:50:04 +0900

    設定画面をstaticな画面として定義、viewModel作成

--- WorkShopSearcher/Components/Setting/ViewModels/SettingViewModel.swift
+++ WorkShopSearcher/Components/Setting/ViewModels/SettingViewModel.swift
@@ -5,6 +5,42 @@
 //  Created by 伊藤凌也 on 2019/07/19.
 //  Copyright © 2019 ry-itto. All rights reserved.
 //
+import RxSwift
+import RxCocoa
 
 final class SettingViewModel {
+    private let disposeBag = DisposeBag()
+    
+    /// 定数
+    /// 時間ピッカーの値一覧
+    let hourPickerValues: [Int] = Array((0...12))
+    
+    /// 分ピッカーの値一覧
+    let minPickerValues: [Int] = Array((0...60))
+    
+    // input
+    let viewDidLoad = PublishRelay<Void>()
+    let setHour = PublishRelay<Int>()
+    let setMin = PublishRelay<Int>()
+    
+    // output
+    let hourValue: Driver<Int>
+    let minValue: Driver<Int>
+    
+    init() {
+        let hourValueRelay = BehaviorRelay<Int>(value: 0)
+        let minValueRelay = BehaviorRelay<Int>(value: 0)
+        
+        self.hourValue = hourValueRelay.asDriver()
+        self.minValue = minValueRelay.asDriver()
+        
+        // Binding
+        setHour.asSignal()
+            .emit(to: hourValueRelay)
+            .disposed(by: disposeBag)
+        
+        setMin.asSignal()
+            .emit(to: minValueRelay)
+            .disposed(by: disposeBag)
+    }
 }
--- WorkShopSearcher/Components/Setting/Views/SettingViewController.swift
+++ WorkShopSearcher/Components/Setting/Views/SettingViewController.swift
@@ -7,11 +7,24 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-final class SettingViewController: UIViewController {
+final class SettingViewController: UITableViewController {
+    
+    @IBOutlet weak var hourTextField: UITextField!
+    @IBOutlet weak var minTextField: UITextField!
+    @IBOutlet weak var notificationSwitch: UISwitch!
+    
+    override var tableView: UITableView! {
+        didSet {
+            tableView.tableFooterView = UIView()
+        }
+    }
     
     static func instantiateWithTabbarItem() -> UINavigationController {
-        let svc = SettingViewController()
+        let storyboard = UIStoryboard(name: "SettingViewController", bundle: nil)
+        let svc = storyboard.instantiateViewController(withIdentifier: "SettingViewController") as! SettingViewController
         svc.title = "設定"
         let nc = UINavigationController(rootViewController: svc)
         nc.title = "設定"
