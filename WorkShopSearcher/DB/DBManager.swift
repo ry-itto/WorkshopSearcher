2a7346e449a3b5d502b82bd99f65dbfadc380af2
ryoya_ito <horusu.dqx@gmail.com>
2019-08-04 22:49:23 +0900

    DBManagerをテスタブルな構造に変更

--- WorkShopSearcher/DB/DBManager.swift
+++ WorkShopSearcher/DB/DBManager.swift
@@ -10,7 +10,45 @@ import RxSwift
 import RxCocoa
 import RealmSwift
 
-final class DBManager {
+protocol DBManagerProtocol {
+    /// いいねイベント登録
+    ///
+    /// - Parameter item: イベントモデル
+    /// - Returns: Observable
+    func create(item: LikeEvent) -> Observable<Void>
+    
+    /// いいねイベント削除
+    ///
+    /// - Parameter item: イベントモデル
+    /// - Returns: Obseevable
+    func delete(item: LikeEvent) -> Observable<Void>
+    
+    /// 指定したいいねイベントのlikedパラメータを反転
+    ///
+    /// - Parameter item: イベントモデル
+    /// - Returns: Observable
+    func toggleLikeState(item: LikeEvent) -> Observable<Void>
+    
+    /// 引数に与えたモデルが登録されているか判定
+    ///
+    /// - Parameter item: イベントモデル
+    /// - Returns: 登録されている: true, 登録されていない: false
+    func isExisted(item: LikeEvent) -> Bool
+    
+    /// 引数に与えたモデルがいいねされているものか判定
+    /// 存在しない場合falseを返却する
+    ///
+    /// - Parameter item: イベントモデル
+    /// - Returns: いいねされている: true, いいねされていない: false
+    func isLiked(item: LikeEvent) -> Bool
+    
+    /// 全てのいいねしたイベント情報を取得
+    ///
+    /// - Returns: 全てのいいねしたイベント情報
+    func fetchAllLikeEvents() -> Observable<[LikeEvent]>
+}
+
+final class DBManager: DBManagerProtocol {
     
     let realm: Realm = try! Realm()
     
@@ -24,10 +62,6 @@ final class DBManager {
         print(Realm.Configuration.defaultConfiguration.fileURL!)
     }
     
-    /// いいねイベント登録
-    ///
-    /// - Parameter item: イベントモデル
-    /// - Returns: Observable
     func create(item: LikeEvent) -> Observable<Void> {
         return Observable.create { [unowned self] observer -> Disposable in
             let maxId = self.realm.objects(LikeEvent.self).sorted(byKeyPath: "id").last?.id ?? 0
@@ -44,10 +78,6 @@ final class DBManager {
         }
     }
     
-    /// いいねイベント削除
-    ///
-    /// - Parameter item: イベントモデル
-    /// - Returns: Obseevable
     func delete(item: LikeEvent) -> Observable<Void> {
         let object = realm.objects(LikeEvent.self).filter("urlString == %@", item.urlString).first!
         return Observable.create { [unowned self] observer -> Disposable in
@@ -63,10 +93,6 @@ final class DBManager {
         }
     }
     
-    /// 指定したいいねイベントのlikedパラメータを反転
-    ///
-    /// - Parameter item: イベントモデル
-    /// - Returns: Observable
     func toggleLikeState(item: LikeEvent) -> Observable<Void> {
         let object = realm.objects(LikeEvent.self).filter("urlString == %@", item.urlString).first!
         return Observable.create { [unowned self] observer -> Disposable in
@@ -82,10 +108,6 @@ final class DBManager {
         }
     }
     
-    /// 引数に与えたモデルが登録されているか判定
-    ///
-    /// - Parameter item: イベントモデル
-    /// - Returns: 登録されている: true, 登録されていない: false
     func isExisted(item: LikeEvent) -> Bool {
         let object = realm.objects(LikeEvent.self).filter("urlString == %@", item.urlString).first
         switch object {
@@ -96,19 +118,11 @@ final class DBManager {
         }
     }
     
-    /// 引数に与えたモデルがいいねされているものか判定
-    /// 存在しない場合falseを返却する
-    ///
-    /// - Parameter item: イベントモデル
-    /// - Returns: いいねされている: true, いいねされていない: false
     func isLiked(item: LikeEvent) -> Bool {
         guard let object = realm.objects(LikeEvent.self).filter("urlString == %@", item.urlString).first else { return false }
         return object.liked
     }
     
-    /// 全てのいいねしたイベント情報を取得
-    ///
-    /// - Returns: 全てのいいねしたイベント情報
     func fetchAllLikeEvents() -> Observable<[LikeEvent]> {
         let objects = realm.objects(LikeEvent.self).filter("liked == true")
         return Observable.create { observer -> Disposable in
